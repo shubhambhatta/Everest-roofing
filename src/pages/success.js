abc7fd5626a70703f82e159d74ec54729669c719
shubham <shubhambhatta@gmail.com>
2020-07-08 23:08:29 +0545

    success route added

---
+++ src/pages/success.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+function success() {
+    return (
+        <Layout footerClassName="bottom_0">
+            <h1>
+            Your form submission is successful, we will get back to you very soon.
+            </h1>
+        </Layout>
+    )
+}
+
+export default success
