455cd43c39f366f441e853a9bb247ec86c171e32
shubham <shubhambhatta@gmail.com>
2020-07-12 00:33:17 +0545

    minor css changes

--- src/components/header.js
+++ src/components/header.js
@@ -2,12 +2,19 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 import Logo from './assets/svg/everestRoofingLogo.svg';
+import LogoMobile from './assets/svg/LogoMobile.svg';
 
 const Header = ({ siteTitle }) => (
   <div className="header">
     <div className="logoGroup">
-    <Logo />
-    <p> Everest Roofing {'&'} Plumbing</p>
+      {/* Smaller logo for mobile devices */}
+      <div className="mobile_logo">
+        <LogoMobile />
+      </div>
+      <div className="regular_logo">
+        <Logo />
+      </div>
+      <p> Everest Roofing {'&'} Plumbing</p>
     </div>
     <ul className="header-list">
       <li className="header-items"><Link to="/" activeClassName="active">Home</Link></li>
